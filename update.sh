0799339400e3d3fe90673df0e21e9240950ce514
Vitalii Morgunov <utlab13@gmail.com>
2025-02-01 21:08:29 +0100

    Add change directory

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ $HOME/opt/dcdn/pop --refresh
 
 sudo systemctl start pop
 
+cd $HOME
+
 echo "-----------------------------------------------------------------------------"
 echo "Проверка логов"
 echo "journalctl -n 100 -f -u pop -o cat"
